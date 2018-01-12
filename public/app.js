99da7a91eeba01b10b58823ef64ff547897715ee
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2018-01-12 12:29:09 -0500

    Edited index.html into public folder

--- public/app.js
+++ public/app.js
@@ -1,5 +1,15 @@
 const app = angular.module('MyMoviesApp', []);
 app.controller('MainController', ['$http', function($http){
   this.whatevs = 'whatevs'
+  // 
+  // $http({
+  //   method: 'GET',
+  //   url: 'http://localhost:3000/actors',
+  // }).then(response => {
+  //   console.log('the response is...', response);
+  //   this.users = response.data;
+  // }).catch(reject => {
+  //   console.log('the rejection is...', reject);
+  // });
 
 }]);
