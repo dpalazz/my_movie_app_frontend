1c731f5f98869c30f81974a930fe2edbc2fdc44a
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2018-01-12 12:41:35 -0500

    Angular linked

--- public/app.js
+++ public/app.js
@@ -1,28 +1,52 @@
 const app = angular.module('MyMoviesApp', []);
 app.controller('MainController', ['$http', function($http){
   this.whatevs = 'whatevs'
+  console.log(this.whatevs);
+// ================
+// Index Route
+// ================
 
-  $http({
-    method: 'GET',
-    url: 'http://localhost:3000/actors',
-  }).then(response => {
-    console.log('the response is...', response);
-    this.users = response.data;
-  }).catch(reject => {
-    console.log('the rejection is...', reject);
-  });
+  // $http({
+  //   method: 'GET',
+  //   url: 'http://localhost:3000/actors',
+  // }).then(response => {
+  //   console.log('the response is...', response);
+  //   this.users = response.data;
+  // }).catch(reject => {
+  //   console.log('the rejection is...', reject);
+  // });
 
-  this.processForm = () => {
-    console.log('Form data: ', this.formdata);
-    $http({
-      method: 'POST',
-      url: 'http://localhost:3000/users',
-      data: this.formdata
-    }).then(response => {
-      console.log('response: ', response);
-      this.users.unshift(response.data);
-    }).catch(reject => {
-      console.log('reject: ', reject);
-    });
+// ==========================
+// Create/Process Form Route
+// ==========================
+
+  // this.processForm = () => {
+  //   console.log('Form data: ', this.formdata);
+  //   $http({
+  //     method: 'POST',
+  //     url: 'http://localhost:3000/users',
+  //     data: this.formdata
+  //   }).then(response => {
+  //     console.log('response: ', response);
+  //     this.users.unshift(response.data);
+  //   }).catch(reject => {
+  //     console.log('reject: ', reject);
+  //   });
+
+// ============
+// Edit Route
+// ============
+    //
+    // this.editDates = (x) => {
+    //   this.currentDatesEdit = x;
+    //   $http({
+    //     method: 'PUT',
+    //     url: 'http://localhost:3000/users/' + this.currentDatesEdit._id,
+    //     data: this.currentDatesEdit
+    //   }).then((response) => {
+    //     const updateByIndex = this.dates.findIndex(x => x._id === this.edittedData._id)
+    //     this.dates[updateByIndex] = this.edittedData;
+    //   }).catch(err => console.error('Catch', err));
+    // };
 
   }]);
