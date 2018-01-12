82f458d659e17331626bcd9d5e364392b0f267df
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2018-01-12 12:32:33 -0500

    Added http routes in appjs

--- public/app.js
+++ public/app.js
@@ -1,15 +1,28 @@
 const app = angular.module('MyMoviesApp', []);
 app.controller('MainController', ['$http', function($http){
   this.whatevs = 'whatevs'
-  // 
-  // $http({
-  //   method: 'GET',
-  //   url: 'http://localhost:3000/actors',
-  // }).then(response => {
-  //   console.log('the response is...', response);
-  //   this.users = response.data;
-  // }).catch(reject => {
-  //   console.log('the rejection is...', reject);
-  // });
 
-}]);
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3000/actors',
+  }).then(response => {
+    console.log('the response is...', response);
+    this.users = response.data;
+  }).catch(reject => {
+    console.log('the rejection is...', reject);
+  });
+
+  this.processForm = () => {
+    console.log('Form data: ', this.formdata);
+    $http({
+      method: 'POST',
+      url: 'http://localhost:3000/users',
+      data: this.formdata
+    }).then(response => {
+      console.log('response: ', response);
+      this.users.unshift(response.data);
+    }).catch(reject => {
+      console.log('reject: ', reject);
+    });
+
+  }]);
