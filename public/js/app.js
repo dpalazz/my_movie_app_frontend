c21a9922d1fa36dff4ad56b9f10f7ba908b86eb5
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2018-01-17 11:15:18 -0500

    Modal fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 const app = angular.module('MyMoviesApp', []);
 
 app.controller('MainController', ['$http', function($http){
-  this.url= 'https://laventure-cinema.herokuapp.com/'
+  this.url= 'http://localhost:3000'
   this.addForm = false;
   this.editModal = false;
   this.addMovie = () => {
