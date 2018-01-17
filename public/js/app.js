410682bf15d6eb599ca01d1502fcce5c9513cf6f
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2018-01-17 12:06:49 -0500

    Edit and Delete routes display right movies

--- public/js/app.js
+++ public/js/app.js
@@ -49,13 +49,13 @@ app.controller('MainController', ['$http', function($http){
   this.createForm = {}
 
   this.processEditForm = (movie) => {
+    console.log(movie);
     $http({
       url: this.url + '/movies/' + movie.id,
       method: 'PUT',
       data: this.createForm
     }).then(response => {
-      const updateByIndex = this.movies.findIndex(movie => movie._id === movie.id)
-      this.movies.splice(updateByIndex, 1, response.data)
+      this.getMovies();
       this.createForm = {};
       this.editModal = false;
     }, error => {
@@ -72,8 +72,7 @@ app.controller('MainController', ['$http', function($http){
       url: this.url + '/movies/' + id,
       method: 'DELETE'
     }).then(response => {
-      const removeMovie = this.movies.findIndex(movie => movie._id === id);
-      this.movies.splice(removeMovie, 1);
+      this.getMovies();
     }, error => {
       console.log(error.message);
     }).catch(err => console.log(err))
@@ -82,7 +81,7 @@ app.controller('MainController', ['$http', function($http){
   // ==============
   // Login Route
   // ==============
-  
+
   this.registerUser = (id) => {
 
     const newUser = {
@@ -107,7 +106,7 @@ app.controller('MainController', ['$http', function($http){
   this.logout = () => {
     this.user = null;
   }
-  
+
   // ==============
   // Register Route
   // ==============
